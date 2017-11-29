56053111ae76a815c8cc5afef33a19885d653ac5
ivan tkachenko <ratijas@users.noreply.github.com>
2017-11-30 01:51:18 +0300

    extract parser for comma-separated items

--- src/dropSQL/ast/insert_into.py
+++ src/dropSQL/ast/insert_into.py
@@ -5,6 +5,7 @@ from dropSQL.parser.streams import *
 from dropSQL.parser.tokens import *
 from .ast import AstStmt, FromSQL
 from .expression import Expression
+from .comma_separated import CommaSeparated
 
 ValueType = List[Expression]
 ValuesType = List[ValueType]
@@ -115,23 +116,3 @@ class ValueFromSQL(FromSQL[ValueType]):
         if not t: return IErr(t.err().empty_to_incomplete())
 
         return IOk(value)
-
-
-class CommaSeparated(Generic[T], Stream[T]):
-    def __init__(self, ty: Type[FromSQL[T]], tokens: Stream[Token]):
-        super().__init__()
-
-        self.ty = ty
-        self.tokens = tokens
-        self.first = True
-
-    def next_impl(self) -> IResult[T]:
-        if self.first:
-            self.first = False
-
-        else:
-            t = self.tokens.peek().and_then(Cast(Comma))
-            if not t: return IErr(Empty())
-            self.tokens.next()
-
-        return self.ty.from_sql(self.tokens)
--- src/tests/from_sql/insert_into.py
+++ src/tests/from_sql/insert_into.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 
 from dropSQL.ast import *
-from dropSQL.ast.insert_into import CommaSeparated, IdentFromSQL
 from dropSQL.parser.streams import *
 
 
@@ -12,10 +11,3 @@ class InsertIntoTestCase(TestCase):
         self.assertTrue(t)
         i = t.ok()
         self.assertEqual(len(i.values), 3)
-
-    def test_separated(self) -> None:
-        sql = 'a, b, c'
-        ts = Tokens.from_str(sql)
-        cs = CommaSeparated(IdentFromSQL, ts)
-        tokens = cs.collect().ok()
-        self.assertEqual(3, len(tokens))
